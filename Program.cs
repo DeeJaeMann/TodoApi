26f38c063222f0a4b16db1fdf3283b111962d52e
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-09-09 12:29:17 -0400

    Implemented TypedResults

--- Program.cs
+++ Program.cs
@@ -2,8 +2,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace TodoApi
 {
+    /// <summary>
+    /// Todo API example
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Main program entry point
+        /// </summary>
+        /// <param name="args">Command line Args</param>
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
@@ -70,6 +77,8 @@ namespace TodoApi
             // Sets up the group to use the URL prefix /todoitems
             var todoItems = app.MapGroup("/todoitems");
 
+            /* Defining endpoints using Results
+             * 
             // Get Endpoints
             todoItems.MapGet("/", async (TodoDb db) =>
                 await db.Todos.ToListAsync());
@@ -119,8 +128,72 @@ namespace TodoApi
 
                 return Results.NotFound();
             });
+            */
+
+            // Define endpoints using TypedResults
+            // Improves testability and automatically returns the response type metadata for OpenAPI
+            // to describe the endpoint
+            // Calls methods instead of lambdas
+
+            todoItems.MapGet("/", GetAllTodos);
+            todoItems.MapGet("/complete", GetCompleteTodos);
+            todoItems.MapGet("/{id}", GetTodo);
+            todoItems.MapPost("/", CreateTodo);
+            todoItems.MapPut("/{id}", UpdateTodo);
+            todoItems.MapDelete("/{id}", DeleteTodo);
 
             app.Run();
+
+            // Endpoint methods
+            static async Task<IResult> GetAllTodos(TodoDb db)
+            {
+                return TypedResults.Ok(await db.Todos.ToArrayAsync());
+            }
+
+            static async Task<IResult> GetCompleteTodos(TodoDb db)
+            {
+                return TypedResults.Ok(await db.Todos.Where(task => task.IsComplete).ToListAsync());
+            }
+
+            static async Task<IResult> GetTodo(int id, TodoDb db)
+            {
+                return await db.Todos.FindAsync(id)
+                    is Todo todo
+                        ? TypedResults.Ok(todo)
+                        : TypedResults.NotFound();
+            }
+
+            static async Task<IResult> CreateTodo(Todo todo, TodoDb db)
+            {
+                db.Todos.Add(todo);
+                await db.SaveChangesAsync();
+
+                return TypedResults.Created($"/todoitems/{todo.Id}", todo);
+            }
+
+            static async Task<IResult> UpdateTodo(int id, Todo inputTodo, TodoDb db)
+            {
+                var todo = await db.Todos.FindAsync(id);
+
+                if (todo is null) return TypedResults.NotFound();
+
+                todo.Name = inputTodo.Name;
+                todo.IsComplete = inputTodo.IsComplete;
+
+                await db.SaveChangesAsync();
+                return TypedResults.NoContent();
+            }
+
+            static async Task<IResult> DeleteTodo(int id, TodoDb db)
+            {
+                if(await db.Todos.FindAsync(id) is Todo todo)
+                {
+                    db.Todos.Remove(todo);
+                    await db.SaveChangesAsync();
+                    return TypedResults.NoContent();
+                }
+                return TypedResults.NotFound();
+            }
         }
     }
 }
