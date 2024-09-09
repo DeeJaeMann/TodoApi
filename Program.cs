e69632a621b619b5a828c71501ea5d754fc78e24
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-09-09 12:46:37 -0400

    Added DTO implementation

--- Program.cs
+++ Program.cs
@@ -75,7 +75,7 @@ namespace TodoApi
             */
 
             // Sets up the group to use the URL prefix /todoitems
-            var todoItems = app.MapGroup("/todoitems");
+            RouteGroupBuilder todoItems = app.MapGroup("/todoitems");
 
             /* Defining endpoints using Results
              * 
@@ -144,6 +144,7 @@ namespace TodoApi
 
             app.Run();
 
+            /* Method definitions without DTO
             // Endpoint methods
             static async Task<IResult> GetAllTodos(TodoDb db)
             {
@@ -187,6 +188,68 @@ namespace TodoApi
             static async Task<IResult> DeleteTodo(int id, TodoDb db)
             {
                 if(await db.Todos.FindAsync(id) is Todo todo)
+                {
+                    db.Todos.Remove(todo);
+                    await db.SaveChangesAsync();
+                    return TypedResults.NoContent();
+                }
+                return TypedResults.NotFound();
+            }
+            */
+
+            // Methods using DTO
+            // Hides the secret field
+            // Endpoint methods
+            static async Task<IResult> GetAllTodos(TodoDb db)
+            {
+                return TypedResults.Ok(await db.Todos.Select(x => new TodoItemDTO(x)).ToArrayAsync());
+            }
+
+            static async Task<IResult> GetCompleteTodos(TodoDb db)
+            {
+                return TypedResults.Ok(await db.Todos.Where(task => task.IsComplete).Select(x => new TodoItemDTO(x)).ToListAsync());
+            }
+
+            static async Task<IResult> GetTodo(int id, TodoDb db)
+            {
+                return await db.Todos.FindAsync(id)
+                    is Todo todo
+                        ? TypedResults.Ok(new TodoItemDTO(todo))
+                        : TypedResults.NotFound();
+            }
+
+            static async Task<IResult> CreateTodo(TodoItemDTO todoItemDTO, TodoDb db)
+            {
+                var todoItem = new Todo
+                {
+                    IsComplete = todoItemDTO.IsComplete,
+                    Name = todoItemDTO.Name
+                };
+
+                db.Todos.Add(todoItem);
+                await db.SaveChangesAsync();
+
+                todoItemDTO = new TodoItemDTO(todoItem);
+
+                return TypedResults.Created($"/todoitems/{todoItem.Id}", todoItemDTO);
+            }
+
+            static async Task<IResult> UpdateTodo(int id, TodoItemDTO todoItemDTO, TodoDb db)
+            {
+                var todo = await db.Todos.FindAsync(id);
+
+                if (todo is null) return TypedResults.NotFound();
+
+                todo.Name = todoItemDTO.Name;
+                todo.IsComplete = todoItemDTO.IsComplete;
+
+                await db.SaveChangesAsync();
+                return TypedResults.NoContent();
+            }
+
+            static async Task<IResult> DeleteTodo(int id, TodoDb db)
+            {
+                if (await db.Todos.FindAsync(id) is Todo todo)
                 {
                     db.Todos.Remove(todo);
                     await db.SaveChangesAsync();
--- Todo.cs
+++ Todo.cs
@@ -8,5 +8,8 @@
         public int Id { get; set; }
         public string? Name { get; set; }
         public bool IsComplete { get; set; }
+        // Demonstrating DTO approach
+        // Data Transfer Object to restrict data that can be input and returned
+        public string? Secret { get; set; }
     }
 }
